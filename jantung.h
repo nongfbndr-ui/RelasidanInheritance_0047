4faa36ac1bc511228ca3cb7b2ffda7f617020bd9
Moreno <nongfbndr@gmail.com>
2026-05-11 11:40:20 +0700

    buat class anak

---
+++ jantung.h
@@ -0,0 +1,15 @@
+#ifndef JANTUG_H
+#define JANTUNG_H
+
+using namespace std;
+
+class jantung {
+public:
+    jantung(){
+        cout << "Jantung dihidupkan\n";
+    }
+    ~jantung() {
+        cout << "Jantung dimatikan\n";
+    }
+};
+#endif
